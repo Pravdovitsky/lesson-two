6019bc20b8ab11dca0080f340ce4c2cfb5155798
Pravdovitsky <chuckmashine@gmail.com>
2020-01-17 19:09:16 +0300

    Lesson two, all tasks

--- task-two/script.js
+++ task-two/script.js
@@ -1,7 +1,7 @@
 const num = +prompt('Введите число', '');
 
 if (num < 5) {
-    alert("Слишком маленькое число!");
+    alert('Слишком маленькое число!');
 } else {
     for (let i = 0; i <= num; i++) {
         console.log(i);
